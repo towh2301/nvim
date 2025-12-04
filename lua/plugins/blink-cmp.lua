123da0579fe5af331a81280cfac7c6e6fca03831
Bui Hoang Huy <huy.buihoang2301@gmail.com>
2025-12-04 15:17:17 +0700

    update ts-auto-tag and treesitter

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,68 @@
+-- File: ~/.config/nvim/lua/plugins/blink-cmp.lua
+return {
+  "saghen/blink.cmp",
+  lazy = false, -- bật ngay khi khởi động (LazyVim sẽ tự load ở InsertEnter)
+  version = "*", -- luôn dùng phiên bản mới nhất
+  build = "cargo build --release",
+
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+  },
+
+  opts = {
+    keymap = {
+      -- Đây là preset ĐƯỢC KHUYẾN CÁO MẠNH NHẤT trong cộng đồng LazyVim 2025
+      preset = "super-tab", -- Tab để chọn + accept, giống VSCode 100%
+
+      -- Nếu bạn vẫn thích Enter để accept thì đổi thành "enter"
+      -- preset = "enter",
+
+      -- Một vài custom nhẹ (không bắt buộc, preset đã rất tốt rồi)
+      ["<C-space>"] = { "show", "fallback" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+    },
+
+    appearance = {
+      nerd_font_variant = "mono", -- đẹp và thẳng hàng nhất
+    },
+
+    completion = {
+      accept = {
+        auto_brackets = { enabled = true }, -- tự động thêm ), ], } khi accept
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 100,
+      },
+      ghost_text = { enabled = true },
+    },
+
+    sources = {
+      -- LSP và path luôn ở trên cùng → ưu tiên cao nhất
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+
+    fuzzy = {
+      -- Rust matcher cực nhanh + chịu lỗi chính tả cực tốt
+      implementation = "prefer_rust_with_warning",
+    },
+
+    -- Cmdline (: / ?) cũng có completion ngon lành
+    cmdline = {
+      enabled = true,
+      keymap = { preset = "cmdline" },
+    },
+  },
+
+  -- Phần này bắt buộc để LazyVim hiểu đây là plugin có opts
+  config = function(_, opts)
+    -- Nếu dùng LazyVim extras (có LazyVim.cmp.expand)
+    if LazyVim and opts.snippets == nil then
+      opts.snippets = { preset = "default", expand = LazyVim.cmp.expand }
+    end
+
+    require("blink.cmp").setup(opts)
+  end,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,4 @@
 return {
-  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
-
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -41,36 +39,7 @@ return {
         "toml",
       },
 
-      -- matchup = {
-      -- 	enable = true,
-      -- },
 
-      -- https://github.com/nvim-treesitter/playground#query-linter
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-      },
-
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = true, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
-      },
-    },
     config = function(_, opts)
       local TS = require("nvim-treesitter")
       TS.setup(opts)
@@ -83,5 +52,22 @@ return {
       })
       vim.treesitter.language.register("markdown", "mdx")
     end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "html",
+      "vue",
+    },
+
+    opts = {
+      enable = true
+    },
   },
 }
